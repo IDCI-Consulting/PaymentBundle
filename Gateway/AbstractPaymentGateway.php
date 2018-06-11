b38837ed1bb283324f966ce52ae0b96427ff1185
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-06-11 10:47:26 +0200

    Update: AbstractPaymentGateway

--- Gateway/AbstractPaymentGateway.php
+++ Gateway/AbstractPaymentGateway.php
@@ -2,10 +2,52 @@
 
 namespace IDCI\Bundle\PaymentBundle\Gateway;
 
+use IDCI\Bundle\PaymentBundle\Entity\Payment;
+use IDCI\Bundle\PaymentBundle\Entity\PaymentGatewayConfiguration;
+use IDCI\Bundle\PaymentBundle\Payment\PaymentFactory;
+
 abstract class AbstractPaymentGateway implements PaymentGatewayInterface
 {
-    public function getParameterNames(): ?array
-    {
-        return get_object_vars($this);
+    //private $returnUrl;
+
+    /**
+     * @var PaymentFactory
+     */
+    protected $paymentFactory;
+
+    /**
+     * @var PaymentGatewayConfiguration
+     */
+    protected $paymentGatewayConfiguration;
+
+    /**
+     * @var Payment
+     */
+    protected $payment;
+
+    public function __construct(
+        PaymentFactory $paymentFactory,
+        PaymentGatewayConfiguration $paymentGatewayConfiguration,
+        ?Payment $payment = null
+    ) {
+        $this->paymentFactory = $paymentFactory;
+        $this->paymentGatewayConfiguration = $paymentGatewayConfiguration;
+        $this->payment = $payment;
     }
+
+    public function createPayment(?array $parameters): Payment
+    {
+        if (null === $this->payment) {
+            $this->payment = $this->paymentFactory
+                ->create($parameters)
+                ->setGatewayConfigurationAlias($this->paymentGatewayConfiguration->getAlias())
+            ;
+        }
+
+        return $this->payment;
+    }
+
+    abstract public function buildHTMLView(): string;
+
+    abstract public static function getParameterNames(): ?array;
 }
