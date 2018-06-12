cfa25c1afbde4b7d622d4d50fc20dd36a00f85e4
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-06-12 16:19:55 +0200

    Fix: remove payment attribute of AbstractPaymentGateway & to move hydratation of paymentGatewayConfugration from controller to a method

--- Gateway/AbstractPaymentGateway.php
+++ Gateway/AbstractPaymentGateway.php
@@ -5,7 +5,6 @@ namespace IDCI\Bundle\PaymentBundle\Gateway;
 use Doctrine\Common\Persistence\ObjectManager;
 use IDCI\Bundle\PaymentBundle\Entity\Payment;
 use IDCI\Bundle\PaymentBundle\Entity\PaymentGatewayConfiguration;
-use IDCI\Bundle\PaymentBundle\Exception\AlreadyDefinedPaymentException;
 use IDCI\Bundle\PaymentBundle\Payment\PaymentFactory;
 
 abstract class AbstractPaymentGateway implements PaymentGatewayInterface
@@ -15,44 +14,33 @@ abstract class AbstractPaymentGateway implements PaymentGatewayInterface
      */
     protected $paymentGatewayConfiguration;
 
-    /**
-     * @var Payment
-     */
-    protected $payment;
-
-    public function __construct(
-        ObjectManager $om,
-        PaymentGatewayConfiguration $paymentGatewayConfiguration
-    ) {
+    public function __construct(ObjectManager $om)
+    {
         $this->om = $om;
-        $this->paymentGatewayConfiguration = $paymentGatewayConfiguration;
     }
 
-    public function setPayment(Payment $payment): self
-    {
-        $this->payment = $payment;
+    public function setPaymentGatewayConfiguration(
+        PaymentGatewayConfiguration $paymentGatewayConfiguration
+    ): PaymentGatewayInterface {
+        $this->paymentGatewayConfiguration = $paymentGatewayConfiguration;
 
         return $this;
     }
 
     public function createPayment(?array $parameters): Payment
     {
-        if (null !== $this->payment) {
-            throw new AlreadyDefinedPaymentException('You can\'t define a payment twice');
-        }
-
-        $this->payment = PaymentFactory::getInstance()
+        $payment = PaymentFactory::getInstance()
             ->create($parameters)
             ->setGatewayConfigurationAlias($this->paymentGatewayConfiguration->getAlias())
         ;
 
-        $this->om->persist($this->payment);
+        $this->om->persist($payment);
         $this->om->flush();
 
-        return $this->payment;
+        return $payment;
     }
 
-    abstract public function buildHTMLView(): string;
+    abstract public function buildHTMLView(Payment $payment): string;
 
     abstract public static function getParameterNames(): ?array;
 }
