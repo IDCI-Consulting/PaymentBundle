bf73e7b073a83d8c07c3ae3bb02add9fe5f7e1dc
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-06-19 11:10:33 +0200

    Update: InvalidTransactionException

--- Exception/InvalidTransactionException.php
+++ Exception/InvalidTransactionException.php
@@ -6,14 +6,6 @@ class InvalidTransactionException extends \Exception
 {
     public function __construct($responseCode, $code = 0, \Exception $previous = null)
     {
-        if ('3D_ATTEMPT' === $responseCode) {
-            $message = '3D_ATTEMPT: payment is impossible';
-        } elseif ('3D_ERROR' === $responseCode) {
-            $message = '3D_ERROR: technical error';
-        } else {
-            $message = 'Unknown error';
-        }
-
-        parent::__construct($message, $code, $previous);
+        parent::__construct(sprintf('ERROR TYPE: %s. Transaction aborted !', $responseCode), $code, $previous);
     }
 }
