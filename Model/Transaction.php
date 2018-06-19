28774b9799c80e5f6f30a0e50a5229925756a478
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-06-19 11:12:34 +0200

    Add: Transaction model

---
+++ Model/Transaction.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace IDCI\Bundle\PaymentBundle\Model;
+
+class Transaction
+{
+    const STATUS_CREATED = 'created';
+
+    const STATUS_VALIDATED = 'validated';
+
+    const STATUS_CANCELED = 'canceled';
+
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $gatewayConfigurationAlias;
+
+    /**
+     * @var string
+     */
+    protected $itemId;
+
+    /**
+     * @var string|null
+     */
+    protected $customerId;
+
+    /**
+     * @var string|null
+     */
+    protected $customerEmail;
+
+    /**
+     * @var string
+     */
+    protected $status;
+
+    /**
+     * @var int
+     */
+    protected $amount;
+
+    /**
+     * @var string
+     */
+    protected $currencyCode;
+
+    /**
+     * @var string|null
+     */
+    protected $description;
+
+    /**
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    protected $updatedAt;
+
+    public function __toString(): string
+    {
+        return $this->id;
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getGatewayConfigurationAlias(): string
+    {
+        return $this->gatewayConfigurationAlias;
+    }
+
+    public function setGatewayConfigurationAlias(string $gatewayConfigurationAlias): self
+    {
+        $this->gatewayConfigurationAlias = $gatewayConfigurationAlias;
+
+        return $this;
+    }
+
+    public function getItemId(): ?string
+    {
+        return $this->itemId;
+    }
+
+    public function setItemId(string $itemId): self
+    {
+        $this->itemId = $itemId;
+
+        return $this;
+    }
+
+    public function getCustomerId(): ?string
+    {
+        return $this->customerId;
+    }
+
+    public function setCustomerId(?string $customerId): self
+    {
+        $this->customerId = $customerId;
+
+        return $this;
+    }
+
+    public function getCustomerEmail(): ?string
+    {
+        return $this->customerEmail;
+    }
+
+    public function setCustomerEmail(?string $customerEmail): self
+    {
+        $this->customerEmail = $customerEmail;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCurrencyCode(): ?string
+    {
+        return $this->currencyCode;
+    }
+
+    public function setCurrencyCode(string $currencyCode): self
+    {
+        $this->currencyCode = $currencyCode;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
