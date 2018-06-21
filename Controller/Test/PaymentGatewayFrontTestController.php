234bb1d23077f1cdd1567e619af0a5233d0aa5a7
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-06-21 10:10:40 +0200

    Update: front test controller

--- Controller/Test/PaymentGatewayFrontTestController.php
+++ Controller/Test/PaymentGatewayFrontTestController.php
@@ -2,13 +2,13 @@
 
 namespace IDCI\Bundle\PaymentBundle\Controller\Test;
 
-use IDCI\Bundle\PaymentBundle\Form\PaymentGatewayConfigurationChoiceType;
 use IDCI\Bundle\PaymentBundle\Form\TransactionFormType;
+use IDCI\Bundle\PaymentBundle\Form\Type\PaymentGatewayConfigurationChoiceType;
 use IDCI\Bundle\PaymentBundle\Manager\PaymentManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Form;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -32,7 +32,15 @@ class PaymentGatewayFrontTestController extends Controller
      */
     public function selectAction(Request $request)
     {
-        $form = $this->createForm(PaymentGatewayConfigurationChoiceType::class);
+        $form = $this
+            ->createFormBuilder(null, [
+                'csrf_protection' => false,
+                'method' => 'GET',
+            ])
+            ->add('payment_gateway_configuration_alias', PaymentGatewayConfigurationChoiceType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm()
+        ;
 
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
@@ -40,7 +48,7 @@ class PaymentGatewayFrontTestController extends Controller
                 $data = $form->getData();
 
                 return $this->redirectToRoute('idci_payment_test_paymentgatewayfronttest_configure', [
-                    'gateway_configuration_alias' => $data['gateway_configuration_alias'],
+                    'configuration_alias' => $data['payment_gateway_configuration_alias'],
                 ]);
             }
 
@@ -53,19 +61,30 @@ class PaymentGatewayFrontTestController extends Controller
     }
 
     /**
-     * @Route("/{gateway_configuration_alias}/transaction/configure")
+     * @Route("/{configuration_alias}/transaction/configure")
      * @Method({"GET"})
      */
-    public function configureAction(Request $request, string $gateway_configuration_alias)
+    public function configureAction(Request $request, string $configuration_alias)
     {
-        $form = $this->createForm(TransactionFormType::class);
+        $form = $this->createForm(TransactionFormType::class, null, [
+            'csrf_protection' => false,
+            'method' => 'GET',
+            'payment_gateway_configuration_alias' => $configuration_alias,
+        ]);
 
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
+                $transactionData = $form->getData();
+
                 return $this->redirectToRoute('idci_payment_test_paymentgatewayfronttest_initialize', [
-                    'gateway_configuration_alias' => $gateway_configuration_alias,
-                    'configuration' => $form->getData(),
+                    'configuration_alias' => $configuration_alias,
+                    'item_id' => $transactionData['item_id'],
+                    'amount' => $transactionData['amount'],
+                    'currency_code' => $transactionData['currency_code'],
+                    'customer_id' => $transactionData['customer_id'],
+                    'customer_email' => $transactionData['customer_email'],
+                    'description' => $transactionData['description'],
                 ]);
             }
 
@@ -78,20 +97,18 @@ class PaymentGatewayFrontTestController extends Controller
     }
 
     /**
-     * @Route("/{gateway_configuration_alias}/transaction/initialize")
+     * @Route("/{configuration_alias}/transaction/initialize")
      * @Method({"GET"})
      */
-    public function initializeAction(Request $request, $gateway_configuration_alias)
+    public function initializeAction(Request $request, $configuration_alias)
     {
-        $paymentContext = $this->paymentManager->createPaymentContextByAlias($gateway_configuration_alias);
+        $paymentContext = $this->paymentManager->createPaymentContextByAlias($configuration_alias);
 
-        $configuration = $request->get('configuration');
-        $configuration['amount'] = (int) $configuration['amount'];
-
-        $paymentContext->createTransaction($configuration);
+        $transaction = $paymentContext->createTransaction($request->query->all());
 
         return $this->render('@IDCIPaymentBundle/Resources/views/create.html.twig', [
             'view' => $paymentContext->buildHTMLView(),
+            'transaction' => $transaction,
         ]);
     }
 }
