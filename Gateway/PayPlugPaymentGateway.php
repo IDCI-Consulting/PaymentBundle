3caa5405beba9f0689a4e81b92d883d24c89ba5e
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-06-28 10:52:25 +0200

    Add: (wip) payplug payment gateway

---
+++ Gateway/PayPlugPaymentGateway.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace IDCI\Bundle\PaymentBundle\Gateway;
+
+use IDCI\Bundle\PaymentBundle\Model\GatewayResponse;
+use IDCI\Bundle\PaymentBundle\Model\PaymentGatewayConfigurationInterface;
+use IDCI\Bundle\PaymentBundle\Model\Transaction;
+use IDCI\Bundle\PaymentBundle\Payment\PaymentStatus;
+use Symfony\Component\HttpFoundation\Request;
+
+class PayPlugPaymentGateway extends AbstractPaymentGateway
+{
+    public function initialize(
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration,
+        Transaction $transaction
+    ): array {
+        return [
+            'clientId' => $paymentGatewayConfiguration->get('client_id'),
+            'transaction' => $transaction,
+            'url' => $this->getCallbackURL($paymentGatewayConfiguration->getAlias()),
+        ];
+    }
+
+    public function buildHTMLView(
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration,
+        Transaction $transaction
+    ): string {
+        $initializationData = $this->initialize($paymentGatewayConfiguration, $transaction);
+
+        return $this->templating->render('@IDCIPaymentBundle/Resources/views/Gateway/paypal.html.twig', [
+            'initializationData' => $initializationData,
+        ]);
+    }
+
+    public function getResponse(
+        Request $request,
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration
+    ): GatewayResponse {
+        $gatewayResponse = (new GatewayResponse())
+            ->setDate(new \DateTime())
+            ->setStatus(PaymentStatus::STATUS_FAILED)
+        ;
+
+        return $gatewayResponse->setStatus(PaymentStatus::STATUS_APPROVED);
+    }
+
+    public static function getParameterNames(): ?array
+    {
+        return [
+            'secret_key',
+        ];
+    }
+}
