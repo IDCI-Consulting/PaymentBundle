e356da6c53ef2d99e06b398a01d9d128b9ac7c92
Benjamin BOUDIER <benjamin.boudier@idci-consulting.fr>
2018-07-03 09:18:04 +0200

    Fix: useless controller in exception

--- Exception/AlreadyDefinedTransactionException.php
+++ Exception/AlreadyDefinedTransactionException.php
@@ -4,8 +4,4 @@ namespace IDCI\Bundle\PaymentBundle\Exception;
 
 class AlreadyDefinedTransactionException extends \Exception
 {
-    public function __construct($message, $code = 0, \Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
