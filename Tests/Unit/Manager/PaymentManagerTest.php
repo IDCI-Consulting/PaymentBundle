0a37112564e04374c7fe6dbfdb8197a07ecc4a06
Jeremy <jeje9769@live.fr>
2018-07-24 16:57:08 +0200

    Added TransactionFactory and Payment manager unit test

---
+++ Tests/Unit/Manager/PaymentManagerTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace IDCI\Bundle\PaymentBundle\Tests\Manager;
+
+use PHPUnit\Framework\TestCase;
+use IDCI\Bundle\PaymentBundle\Manager\PaymentManager;
+use Doctrine\Common\Persistence\ObjectManager;
+use IDCI\Bundle\PaymentBundle\Entity\PaymentGatewayConfiguration;
+use IDCI\Bundle\PaymentBundle\Exception\NoPaymentGatewayConfigurationFoundException;
+use IDCI\Bundle\PaymentBundle\Gateway\PaymentGatewayRegistryInterface;
+use IDCI\Bundle\PaymentBundle\Manager\TransactionManagerInterface;
+use IDCI\Bundle\PaymentBundle\Payment\PaymentContext;
+use IDCI\Bundle\PaymentBundle\Payment\PaymentContextInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class PaymentContextTest extends TestCase
+{
+    /**
+     * @var PaymentManager
+     */
+    private $paymentManager;
+
+    /**
+     * @var ObjectManager
+     */
+    private $om;
+
+    /**
+     * @var PaymentGatewayRegistryInterface
+     */
+    private $paymentGatewayRegistry;
+
+    /**
+     * @var TransactionManagerInterface
+     */
+    private $transactionManager;
+
+    /**
+     * @var EventDispatcher
+     */
+    private $dispatcher;
+
+    private $paymentGatewayConfigurationRepository;
+
+    public function setUp()
+    {
+        $this->transactionManager = $this->getMockBuilder(TransactionManagerInterface::class)
+            ->getMock()
+        ;
+
+        $this->dispatcher = $this->getMockBuilder(EventDispatcher::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $this->paymentGatewayConfiguration = $this->getMockBuilder(PaymentGatewayConfiguration::class)
+            ->getMock()
+        ;
+
+        $this->paymentGatewayConfiguration
+            ->method('getGatewayName')
+            ->willReturn('dummy_gateway_name')
+        ;
+
+        $this->paymentGatewayConfigurationRepository = $this->getMockBuilder(EntityRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $this->paymentGatewayRegistry = $this->getMockBuilder(PaymentGatewayRegistryInterface::class)
+            ->getMock()
+        ;
+
+        $this->om = $this->getMockBuilder(ObjectManager::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $this->om
+            ->method('getRepository')
+            ->with(PaymentGatewayConfiguration::class)
+            ->willReturn($this->paymentGatewayConfigurationRepository)
+        ;
+
+        $this->paymentManager = new PaymentManager(
+            $this->om,
+            $this->paymentGatewayRegistry,
+            $this->transactionManager,
+            $this->dispatcher
+        );
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testNotCreatePaymentContextByAlias()
+    {
+        $this->paymentGatewayConfigurationRepository
+            ->method('findOneBy')
+        ;
+        //Wrong alias is passed to the method to throw an exception
+        $this->paymentManager->createPaymentContextByAlias('wrong_payment_gateway_configuration_alias');
+    }
+
+    public function testCreatePaymentContextByAlias()
+    {
+        $this->paymentGatewayConfigurationRepository
+            ->method('findOneBy')
+            ->with(['alias' => 'dummy_gateway_alias'])
+            ->willReturn($this->paymentGatewayConfiguration)
+        ;
+
+        $paymentContext = $this->paymentManager->createPaymentContextByAlias('dummy_gateway_alias');
+        $this->assertInstanceOf(PaymentContext::class, $paymentContext);
+        $this->assertEquals($this->dispatcher, $paymentContext->dispatcher);
+        $this->assertEquals($this->paymentGatewayConfiguration, $paymentContext->getPaymentGatewayConfiguration());
+    }
+}
--- Tests/Unit/Payment/TransactionFactoryTest.php
+++ Tests/Unit/Payment/TransactionFactoryTest.php
@@ -4,6 +4,7 @@ namespace IDCI\Bundle\PaymentBundle\Test\Unit\Payment;
 
 use PHPUnit\Framework\TestCase;
 use IDCI\Bundle\PaymentBundle\Payment\TransactionFactory;
+use IDCI\Bundle\PaymentBundle\Model\Transaction;
 
 class TransactionFactoryTest extends TestCase
 {
@@ -22,5 +23,47 @@ class TransactionFactoryTest extends TestCase
             'description' => 'Dummy description',
             'metadatas' => [],
         ]);
+    }
+
+    /**
+     * @expectedException \Exception
+     */
+    public function testCreateWithWrongAmount()
+    {
+        TransactionFactory::getInstance()->create([
+            'gateway_configuration_alias' => 'dummy_gateway_alias',
+            'item_id' => 'dummy_item_id',
+            'customer_id' => 'dummy_customer_id',
+            'customer_email' => 'dummy_customer_email',
+            'amount' => 'wrong_amount',
+            'currency_code' => 'EUR',
+            'description' => 'Dummy description',
+            'metadatas' => [],
+        ]);
+    }
+
+    public function testCreate()
+    {
+        $transaction = TransactionFactory::getInstance()->create([
+            'gateway_configuration_alias' => 'dummy_gateway_alias',
+            'item_id' => 'dummy_item_id',
+            'customer_id' => 'dummy_customer_id',
+            'customer_email' => 'dummy_customer_email',
+            'amount' => 10,
+            'currency_code' => 'EUR',
+            'description' => 'Dummy description',
+            'metadatas' => [],
+        ]);
+
+        $this->assertInstanceOf(Transaction::class, $transaction);
+        $this->assertEquals('dummy_gateway_alias', $transaction->getGatewayConfigurationAlias());
+        $this->assertEquals('dummy_item_id', $transaction->getItemId());
+        $this->assertEquals('dummy_customer_id', $transaction->getCustomerId());
+        $this->assertEquals('dummy_customer_email', $transaction->getCustomerEmail());
+        $this->assertEquals(10, $transaction->getAmount());
+        $this->assertEquals('EUR', $transaction->getCurrencyCode());
+        $this->assertEquals('Dummy description', $transaction->getDescription());
+        $this->assertInternalType('array', $transaction->getMetadatas());
+        $this->assertEquals(0, count($transaction->getMetadatas()));
     }
 }
