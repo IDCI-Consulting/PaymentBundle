023596718c20835f97179b76ea5cf22e1f99bd94
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2026-04-08 15:25:32 +0200

    Start working on WorldlinePaymentGateway

---
+++ Gateway/WorldlinePaymentGateway.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace IDCI\Bundle\PaymentBundle\Gateway;
+
+use IDCI\Bundle\PaymentBundle\Model\GatewayResponse;
+use IDCI\Bundle\PaymentBundle\Model\PaymentGatewayConfigurationInterface;
+use IDCI\Bundle\PaymentBundle\Model\Transaction;
+use Symfony\Component\HttpFoundation\Request;
+
+class WorldlinePaymentGateway extends AbstractPaymentGateway
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration,
+        Transaction $transaction,
+        array $options = []
+    ): array {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildHTMLView(
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration,
+        Transaction $transaction,
+        array $options = []
+    ): string {
+        $initializationData = $this->initialize($paymentGatewayConfiguration, $transaction);
+
+        return $this->templating->render('@IDCIPayment/Gateway/worldline.html.twig', [
+            'initializationData' => $initializationData,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getReturnResponse(
+        Request $request,
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration
+    ): GatewayResponse {
+        dd('getReturnResponse');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCallbackResponse(
+        Request $request,
+        PaymentGatewayConfigurationInterface $paymentGatewayConfiguration
+    ): GatewayResponse {
+        dd('getCallbackResponse');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getParameterNames(): ?array
+    {
+        return array_merge(
+            parent::getParameterNames(),
+            [
+            ]
+        );
+    }
+}
