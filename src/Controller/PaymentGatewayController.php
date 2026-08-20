4303391155489be0ff20de54f0840f45db66e188
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2026-08-20 12:03:20 +0200

    Add paymentlogger in the controller

--- src/Controller/PaymentGatewayController.php
+++ src/Controller/PaymentGatewayController.php
@@ -3,6 +3,7 @@
 namespace IDCI\Bundle\PaymentBundle\Controller;
 
 use IDCI\Bundle\PaymentBundle\Manager\PaymentManager;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,15 +11,21 @@ use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class PaymentGatewayController extends AbstractController
 {
+    private LoggerInterface $paymentLogger;
     private PaymentManager $paymentManager;
 
-    public function __construct(PaymentManager $paymentManager)
+    public function __construct(LoggerInterface $paymentLogger, PaymentManager $paymentManager)
     {
+        $this->paymentLogger = $paymentLogger;
         $this->paymentManager = $paymentManager;
     }
 
     public function notifyTransaction(Request $request, EventDispatcherInterface $dispatcher, string $configuration_alias)
     {
+        $this->paymentLogger->info('[IDCIPaymentBundle] PaymentGatewayController:notifyTransaction', [
+            'request' => $request,
+        ]);
+
         $paymentContext = $this->paymentManager->createPaymentContext($configuration_alias, $request);
         $paymentContext->retrieveTransaction();
         $paymentContext->handleNotification();
